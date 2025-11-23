6fed5f3eb74927ab5cb52559f67b3c516344e8c0
Justus <justus.d2025@gmail.com>
2025-11-23 21:04:23 +0100

    outsourced from main

---
+++ src/backend/flask_main.py
@@ -0,0 +1,17 @@
+from flask import Flask,jsonify, render_template, request, redirect, url_for
+from src.data import JobsTable, JobIdsTable, JobID, Job, JobApplianceStates
+GET, POST, DELETE = 'GET', 'POST', 'DELETE'
+
+class JobQuery:
+    def __init__(self, query: list[str]):
+        duery = query
+        self.COMPANY = duery['company']
+        self.TITLE_ID = int(duery['job_title'])
+        self.URL = duery['url']
+        self.MAIL = duery['mail']
+        self.PHONE_NUMBER = duery['phone_number']
+        self.DESCRIPTION = duery['description']
+        self.STATE = int(duery['state'])
+
+from src.errors import  http_status_codes
+app = Flask(__name__)
