c04ff06c0b48c1c97a5082b72cfc201b94b81d78
Justus <justus.d2025@gmail.com>
2025-11-23 21:04:29 +0100

    outsourced from main

---
+++ src/backend/techstack.py
@@ -0,0 +1,17 @@
+from src.backend.flask_main import *
+
+@app.route('/tech_stack/create',methods = [DELETE, GET])
+def create_tech_stack():
+    ...
+    
+@app.route('/tech_stack/read',methods = [DELETE, GET])
+def read_tech_stack():
+    ...
+    
+@app.route('/tech_stack/update',methods = [DELETE, GET])
+def update_tech_stack():
+    ...
+
+@app.route('/tech_stack/delete',methods = [DELETE, GET])
+def delete_tech_stack():
+    ...
