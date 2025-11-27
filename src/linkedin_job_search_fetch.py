273747ecf96cc06b27d328e32f3a49eab196e641
Justus <justus.d2025@gmail.com>
2025-11-27 23:20:25 +0100

    add linkedin jobsearch fetch

---
+++ src/linkedin_job_search_fetch.py
@@ -0,0 +1,106 @@
+from html.parser import HTMLParser
+def attr_finder(attrs, search_for) -> list:
+    return [i for i in attrs if i[0] == search_for]
+class IDGetterSearch(HTMLParser):
+    def __init__(self, *, convert_charrefs = True):
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.data_job_ids = []
+    def handle_starttag(self, _, attrs):
+        if attrs: 
+            if attr_finder(attrs, 'data-job-id'):
+                self.data_job_ids.append(attrs[0][1])
+
+class FetchJob(HTMLParser):
+    def __init__(self, *, convert_charrefs = True):
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.logo = None
+        self.job_title = None
+        self.next_is_job_title = False
+        self.company_name = None
+        self.next_is_company_name = False
+        self.company_name_miss = 0
+        self.description = ""
+        self.error = False
+        
+    def feed(self, data):
+        # Find the current job
+        start_index = data.find('<section class="show-more-less-html"')
+        end_index = data[start_index:].find("</div>")
+        if start_index == -1:
+            self.error = True
+            return
+        new_data = data[start_index:end_index+start_index]
+        for old, new in (('<span>',''),('</span>',''),('<br>','\n'), ('<p>',''), ('<strong>',''),('</strong>',''),('</div>',''),('</p>','\n'),('<!---->',''), ('</li>',''), ('<li>','\n* '), ('<ul>',''), ('</ul>','')):
+            new_data = new_data.replace(old, new)
+        new_data = new_data.split('">\n')[-1].strip()
+        self.description = new_data
+
+        return super().feed(data)
+    
+    def handle_starttag(self, tag, attrs):
+        SRC = attr_finder(attrs, 'src')
+        ALT = attr_finder(attrs, 'alt')
+        HREF = attr_finder(attrs, 'href')
+        DATA_DELAYED_URL = attr_finder(attrs, 'data-delayed-url')
+        if tag == 'img':
+            if attrs: 
+                if not self.logo and DATA_DELAYED_URL and ALT and 'company' in DATA_DELAYED_URL[0][1].lower():
+                    # Searches for the company logo
+                    self.logo = DATA_DELAYED_URL[0][1]
+        if tag == 'a':
+            
+            if not self.company_name and HREF and 'company' in HREF[0][1]:
+                self.next_is_company_name = True
+            if HREF and 'company' in HREF[0][1]:
+                #print(HREF[0][1])
+                ...
+        if not self.job_title and tag == 'h1':
+            self.next_is_job_title = True
+
+    def handle_data(self, data):
+        if self.next_is_job_title:
+            # Gets the job title
+            self.job_title = data
+            self.next_is_job_title = False
+        if self.next_is_company_name and self.company_name_miss == 2:
+            if data.strip() and data.strip():
+                self.company_name = data.strip()
+                self.next_is_company_name = False
+        if self.next_is_company_name and self.company_name_miss < 2:
+            self.company_name_miss += 1
+            self.next_is_company_name = False            
+        
+from requests import get
+def get_linkedin_job_site(job_id: int):
+    try:
+        return get(f'https://www.linkedin.com/jobs/view/{job_id}/')._content.decode()
+    except: ...
+
+def fetch_linkedin_job_data(job_id: int) -> dict:
+    """
+    Gets you the logo, job_title, company_name, description & also returns the job_id in the dict.
+    """
+    print(job_id)
+    content = get_linkedin_job_site(job_id)
+    if content is None: 
+        return {'error': 'Get return is none'}
+    with open('test1.html','wb') as f:
+        f.write(content.encode())
+    parser = FetchJob()
+    parser.feed(content)
+    return {
+        'logo': parser.logo, 
+        'job_title': parser.job_title, 
+        'company_name': parser.company_name,
+        'description': parser.description,
+        'job_id': job_id
+    }
+def fetch_job_ids(html: str) -> list:
+    """
+    Takes a html-structure in string form, runs the HTML parser and fetches the job ids from there.
+    For this to work you need to copy the html from your browser and paste this into a file!
+    """
+    parser = IDGetterSearch()
+    parser.feed(html)
+    
+    return parser.data_job_ids
