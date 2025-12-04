160491352026df55549b46ccc99e149a92ae64c3
Justus <justus.d2025@gmail.com>
2025-12-04 15:21:31 +0100

    the new version of the js makes it versatile

--- src/frontend/static/linkedin_create_workaround.js
+++ src/frontend/static/linkedin_create_workaround.js
@@ -1,18 +1,20 @@
-async function send() {
+
+
+async function send(url, redirect) {
     document.getElementById('send-btn').disabled = true;
     const content = document.getElementById('html').value;
     const blob = new Blob([content], { type: 'text/plain' });
     const formData = new FormData();
-    formData.append('file', blob, 'textarea-content.txt');
-    window.location.replace("/load");
-    await fetch('/linkedin/create', {
+    const input = document.getElementById('filename');
+    var name = 'textarea-content.txt'
+    if (input) {
+        name = `${input.value}.txt`
+    }
+    formData.append('file', blob, name);
+    
+    await fetch(url, {
                     method: 'POST',
                     body: formData
-                }).then(e =>{
-                    console.log("test");
-                    window.location.replace("/linkedin");
-                }).catch(e => {
-                    console.log(e);
                 })
-    
+    window.location.replace(redirect);
 }
