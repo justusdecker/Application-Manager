089f19a2570eab1455118db766e426a7a4eafee7
Justus <justus.d2025@gmail.com>
2025-12-04 14:22:04 +0100

    removed the JobQuery

--- src/flask_main.py
+++ src/flask_main.py
@@ -1,22 +1,9 @@
 from flask import Flask,jsonify, render_template, request, redirect, url_for, send_file
 from flask import Request
-GET, POST, DELETE = 'GET', 'POST', 'DELETE'
-
-class JobQuery:
-    def __init__(self, query: list[str]):
-        duery = query
-        self.COMPANY = duery['company']
-        self.TITLE_ID = int(duery['job_title'])
-        self.URL = duery['url']
-        self.MAIL = duery['mail']
-        self.PHONE_NUMBER = duery['phone_number']
-        self.DESCRIPTION = duery['description']
-        self.STATE = int(duery['state'])
-
-from src.constants import  http_status_codes
+from src.constants import  http_status_codes, GET, POST, DELETE
 import os
+
 directory = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 directory = os.path.join(directory, 'Application-Manager', 'src', 'frontend')
 
-print(directory)
-app = Flask(__name__,static_folder=directory + '\\static',template_folder=directory + '\\templates')
+app = Flask(__name__,static_folder=directory + '\\static', template_folder=directory + '\\templates')
