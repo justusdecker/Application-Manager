47d47a482345d08bb33a9029fdc93c5343b5155f
Justus <justus.d2025@gmail.com>
2025-12-04 14:21:10 +0100

    rem old

--- src/help.py
+++
@@ -1,6 +0,0 @@
-import markdown
-from html.parser import HTMLParser
-from src.constants import HELP
-        
-def generate_help() -> str:
-    return HELP
--- src/validation.py
+++
@@ -1,7 +0,0 @@
-
-def integer_or_default(v: str | int, default: int) -> int:
-    if isinstance(v, int): return v
-    if v.isdecimal():
-        return int(v)
-    else:
-        return default
