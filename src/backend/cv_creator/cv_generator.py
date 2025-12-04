21e0381ee56d8ba3c8a05ab7c4474e04dbe0bbda
Justus <justus.d2025@gmail.com>
2025-12-04 14:22:58 +0100

    rad ...

---
+++ src/backend/cv_creator/cv_generator.py
@@ -0,0 +1,29 @@
+from jinja2 import Template
+from src.flask_main import url_for
+PATH = "./src/cv_creator/"
+from src.cv_creator.objects import *
+
+def generate(filepath: str, settings: str) -> str:
+    # Reading
+    loc = {}
+    exec(settings, globals(), loc)
+    with open(f'{PATH}index.html', 'rb') as f:
+        HTML = f.read().decode()
+        
+    with open(f'{PATH}styles.css', 'rb') as f:
+        CSS = f.read().decode()
+    # Rendering
+    template = Template(HTML)
+    pp = url_for('static', filename='profile.png')
+    result = template.render(
+        data = loc['CVC'], 
+        enumerate = enumerate, 
+        css = CSS,
+        profile_path = pp
+    )
+    del loc['CVC']
+    # Writing & Return
+    with open(filepath, 'wb') as f:
+        f.write(result.encode())
+        
+    return result
